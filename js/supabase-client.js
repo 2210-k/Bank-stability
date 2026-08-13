2892805885ddaada6e452a2e7e3ffdef097b5179
2210-k <odula6346@gmail.com>
2026-08-13 13:04:55 +0500

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const supabaseUrl = 'https://bpodhjlqzfxesxjduujz.supabase.co';
-const supabaseAnonKey = 'sb_publishable_EtMfPg9vVMa1la22ADd6fA_vpvRNM8x'; // скопируйте из Settings → API
+const supabaseAnonKey = 'sb_publishable_EtMfPg9vVMa1la22ADd6fA_vpvRNM8x'; // замените на свой
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
